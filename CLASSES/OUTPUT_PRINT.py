5e2749ca6c96710e4b18b8b32cf329c907954391
cfernandesFEUP <cfernandes@fe.up.pt>
2021-12-16 12:38:54 +0000

    Update OUTPUT_PRINT.py

--- CLASSES/OUTPUT_PRINT.py
+++ CLASSES/OUTPUT_PRINT.py
@@ -36,19 +36,19 @@ class PRINTING:
 
         print(dots)
 
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Presure angle \u03B1:', "%.1f" % GTYPE.alpha, '\u00b0'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Helix angle \u03B2:', "%.1f" % GTYPE.beta, '\u00b0'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Module m:', "%.1f" % GTYPE.m, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Number of teeth z1:', "%.1f" % GTYPE.z[0], ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Number of teeth z2:', "%.1f" % GTYPE.z[1], ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Profile shift x1:', "%.4f" % GTYPE.x[0], ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Profile shift x2:', "%.4f" % GTYPE.x[1], ''))
 
         print(dash)
@@ -59,28 +59,28 @@ class PRINTING:
 
         print('Pinion:')
 
-        print('{:<35s}{:<20s}{:<10s}'.format('  Young modulus E1:', "%.0f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Young modulus E1:', "%.0f" %
                                              (GMAT.E1/1e3), 'GPa'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Poisson ratio \u03BD1:', "%.2f" % GMAT.v1, ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Thermal capacity cp1:', "%.2f" % GMAT.cp1, 'J/kg.K'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Thermal conductivity k1:', "%.2f" % GMAT.k1, 'W/m.K'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Density \u03C11:', "%.2f" % GMAT.rho1, 'kg/m3'))
 
         print('Wheel:')
 
-        print('{:<35s}{:<20s}{:<10s}'.format('  Young modulus E2:', "%.0f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Young modulus E2:', "%.0f" %
                                              (GMAT.E2/1e3), 'GPa'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Poisson ratio \u03BD2:', "%.2f" % GMAT.v2, ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Thermal capacity cp2:', "%.2f" % GMAT.cp2, 'J/kg.K'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Thermal conductivity k2:', "%.2f" % GMAT.k2, 'W/m.K'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  Density \u03C12:', "%.2f" % GMAT.rho2, 'kg/m3'))
 
         print(dash)
@@ -89,22 +89,22 @@ class PRINTING:
 
         print(dots)
 
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Axis distance:', "%.1f" % GEO.al, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Base pitch n:', "%.3f" % GEO.pb, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Base pitch t:', "%.3f" % GEO.pbt, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Root radius:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Root radius:', "%.3f" %
                                              GEO.rf1 + ' / ' +
                                              "%.3f" % GEO.rf2, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Reference radius:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Reference radius:', "%.3f" %
                                              GEO.r1 + ' / ' +
                                              "%.3f" % GEO.r2, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Pitch radius:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Pitch radius:', "%.3f" %
                                              GEO.rl1 + ' / ' +
                                              "%.3f" % GEO.rl2, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Tip radius:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Tip radius:', "%.3f" %
                                              GEO.ra1 + ' / ' +
                                              "%.3f" % GEO.ra2, 'mm'))
 
@@ -114,11 +114,11 @@ class PRINTING:
 
         print(dots)
 
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Transverse \u03B5\u03B1:', "%.2f" % GEO.epslon_alpha, ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Overlap \u03B5\u03B2:', "%.2f" % GEO.epslon_beta, ''))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Total \u03B5\u03B3:', "%.2f" % GEO.epslon_gama, ''))
 
         print(dash)
@@ -127,11 +127,11 @@ class PRINTING:
 
         print(dots)
 
-        print('{:<35s}{:<20s}{:<10s}'.format('T1T2:', "%.2f" % GEO.T1T2, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('AB:', "%.2f" % GEO.AB, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('AC:', "%.2f" % GEO.AC, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('AD:', "%.2f" % GEO.AD, 'mm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('AE:', "%.2f" % GEO.AE, 'mm'))
+        print('{:<35s}{:^20s}{:<10s}'.format('T1T2:', "%.2f" % GEO.T1T2, 'mm'))
+        print('{:<35s}{:^20s}{:<10s}'.format('AB:', "%.2f" % GEO.AB, 'mm'))
+        print('{:<35s}{:^20s}{:<10s}'.format('AC:', "%.2f" % GEO.AC, 'mm'))
+        print('{:<35s}{:^20s}{:<10s}'.format('AD:', "%.2f" % GEO.AD, 'mm'))
+        print('{:<35s}{:^20s}{:<10s}'.format('AE:', "%.2f" % GEO.AE, 'mm'))
 
         print(dash)
 
@@ -139,27 +139,27 @@ class PRINTING:
 
         print(dots)
 
-        print('{:<35s}{:<20s}{:<10s}'.format('Pin:', "%.1f" % GFS.Pin, 'W'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Torque T:', "%.1f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Pin:', "%.1f" % GFS.Pin, 'W'))
+        print('{:<35s}{:^20s}{:<10s}'.format('Torque T:', "%.1f" %
                                              GFS.torque1 + ' / ' +
                                              "%.1f" % GFS.torque2, 'N.m'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Speed n:', "%.1f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Speed n:', "%.1f" %
                                              GFS.speed1 + ' / ' +
                                              "%.1f" % GFS.speed2, 'rpm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Angular speed \u03C9:', "%.1f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Angular speed \u03C9:', "%.1f" %
                                              GFS.omega1 + ' / ' +
                                              "%.1f" % GFS.omega2, 'rad/s'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Tangential load F_t:', "%.1f" % GFS.ft, 'N'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Radial load F_r:', "%.1f" % GFS.fr, 'N'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Normal load F_n:', "%.1f" % GFS.fn, 'N'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Axial load F_a:', "%.1f" % GFS.fa, 'N'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Base circle load F_bt:', "%.1f" % GFS.fbt, 'N'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Base circle load F_bn:', "%.1f" % GFS.fbn, 'N'))
 
         print(dash)
@@ -169,28 +169,28 @@ class PRINTING:
         print(dots)
 
         print('Maximum pressure p0:')
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             '  At pitch point:', "%.2f" % GCONTACT.p0I, 'MPa'))
-        print('{:<35s}{:<20s}{:<10s}'.format('  Maximum along AE:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Maximum along AE:', "%.2f" %
                                              GCONTACT.p0.max(), 'MPa'))
-        print('{:<35s}{:<20s}{:<10s}'.format('  Minimum along AE:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Minimum along AE:', "%.2f" %
                                              GCONTACT.p0.min(), 'MPa'))
 
         print('Mean pressure pm:')
-        print('{:<35s}{:<20s}{:<10s}'.format('  At pitch point:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  At pitch point:', "%.2f" %
                                              GCONTACT.pmI, 'MPa'))
-        print('{:<35s}{:<20s}{:<10s}'.format('  Maximum along AE:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Maximum along AE:', "%.2f" %
                                              GCONTACT.pm.max(), 'MPa'))
-        print('{:<35s}{:<20s}{:<10s}'.format('  Minimum along AE:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Minimum along AE:', "%.2f" %
                                              GCONTACT.pm.min(), 'MPa'))
 
         print('Contact half-width aH:')
-        print('{:<35s}{:<20s}{:<10s}'.format('  At pitch point:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  At pitch point:', "%.3f" %
                                              (GCONTACT.aHI*1e3), '\u03BCm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('  Maximum along AE:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Maximum along AE:', "%.3f" %
                                              (GCONTACT.aH.max()*1e3),
                                              '\u03BCm'))
-        print('{:<35s}{:<20s}{:<10s}'.format('  Minimum along AE:', "%.3f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('  Minimum along AE:', "%.3f" %
                                              (GCONTACT.aH.min()*1e3),
                                              '\u03BCm'))
         print(dash)
@@ -199,17 +199,17 @@ class PRINTING:
 
         print(dots)
 
-        print('{:<35s}{:<20s}{:<10s}'.format('Gear Loss Factor HVL:', "%.4f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Gear Loss Factor HVL:', "%.4f" %
                                              GCONTACT.HVL, 'Wimmer'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Gear Loss Factor HV:', "%.4f" % GEO.HV, 'Ohlendorf'))
-        print('{:<35s}{:<20s}{:<10s}'.format(
+        print('{:<35s}{:^20s}{:<10s}'.format(
             'Coefficient of Friction \u03BCmZ:', "%.4f" % GCONTACT.CoF, 'Schlenk'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Average Power Loss Pvzp:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Average Power Loss Pvzp:', "%.2f" %
                                              GCONTACT.Pvzp, 'W'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Maximum Local Power Loss:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Maximum Local Power Loss:', "%.2f" %
                                              GCONTACT.PvzpL.max(), 'W'))
-        print('{:<35s}{:<20s}{:<10s}'.format('Minimum Local Power Loss:', "%.2f" %
+        print('{:<35s}{:^20s}{:<10s}'.format('Minimum Local Power Loss:', "%.2f" %
                                              GCONTACT.PvzpL.min(), 'W'))
 
         print(dash)
